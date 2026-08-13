57e1b094fa96801880da99be07742027b086cc48
domecontent1 <dome.content1@gmail.com>
2026-08-13 17:19:48 +0530

    FIX: Corrected bootstrap and removed duplicate providers

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+// import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+//
+// import { routes } from './app/app.routes';
+// import { AppComponent } from './app/app.component';
+//
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+//     provideIonicAngular(),
+//     provideRouter(routes, withPreloading(PreloadAllModules)),
+//   ],
+// });
 
-import { routes } from './app/app.routes';
+
+// If this fails, use @angular/platform-browser
+import { bootstrapApplication as bootstrapStandalone } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-  ],
-});
+bootstrapStandalone(AppComponent, appConfig)
+  .catch((err) => console.error(err));
