faafa024926fedf42ad7f50303779bbc2cf8393e
domecontent1 <dome.content1@gmail.com>
2026-08-13 11:48:31 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'abacus_ui',
+  webDir: 'www'
+};
+
+export default config;
