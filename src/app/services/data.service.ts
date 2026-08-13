4106613fb6aded18a58576b2b05b3bc3c4e439c4
domecontent1 <dome.content1@gmail.com>
2026-08-13 15:05:59 +0530

    Connected to live Render backend new

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,13 +11,12 @@ export interface Question {
   providedIn: 'root'
 })
 export class DataService {
-  // Replace 'http://127.0.0.1:8000' with your Render URL
   private apiUrl = 'https://abacus-project-backend.onrender.com/api/practice';
 
   constructor(private http: HttpClient) { }
 
   getQuestions(count: number, digits: number, rows: number): Observable<any> {
-    const finalUrl = `${this.apiUrl}?questions=${count}&digits=${digits}&rows=${rows}`;
-    return this.http.get(finalUrl);
+    const requestUrl = `${this.apiUrl}?questions=${count}&digits=${digits}&rows=${rows}`;
+    return this.http.get(requestUrl);
   }
 }
